10a21e025340c878ff7286474ee9d7907439b08e
ronca <roncalara@outlook.com>
2021-06-23 14:22:06 -0700

    base model with 4:1 gear ratio first commit

---
+++ Calculations.m
@@ -0,0 +1,87 @@
+classdef Calculations
+   properties
+      SampleRate = 64; % Hz
+      Gravity = 9.81; % already taken care of in majority of formulas for signage
+      
+      MassArm = 0.04636; %(kg)
+      CGArm = 0.1310; % (m)% measured from rotation point
+      
+      MassBall = 0.145; % (kg)
+      CGBall = 0.20; % (m)% measured from rotation point
+      RadiusBall = 0.0315; % (m)
+
+      X0 {}
+      Y0 {}
+      
+   end
+   methods
+       
+       function launch_x_y_vel = launch_x_y_velocity(self, max_torque, arm_swing_angle, arm_start_angle, gear_ratio)
+           
+          %% various parameters
+          m_arm = self.MassArm;
+          cg_arm = self.CGArm;
+          m_ball = self.MassBall;
+          cg_ball = self.CGBall; 
+          r_ball = self.RadiusBall;
+          d_ball = sqrt(r_ball^2+cg_ball^2);
+          
+          %% inertia for input
+          J_shaft_input = 15.84*0.0001*(1/1000);
+          J_base_gear = 6.94*0.0001*(1/1000);
+          J_1 = J_shaft_input + J_base_gear;
+          
+          %% inertia for output
+          J_shaft_output = 15.64*0.0001*(1/1000);
+          J_arm = (289.3*0.0001*(1/1000)) + (m_arm*cg_arm^2); % kg*m^2
+          J_follower_gear = 818.95*0.0001*(1/1000);
+          J_ball = ((2/5)*m_ball*r_ball^2) + (m_ball*d_ball^2);
+          J_2 = J_arm + J_follower_gear + J_shaft_output + J_ball;
+          
+          %% Total inertia experienced at the input
+          J_total =  J_1 + (J_2/(gear_ratio)^2); 
+          
+          arm_end_angle = arm_start_angle + arm_swing_angle; % degrees
+          arm_swing_angle_rad = (arm_swing_angle)*pi/180; %rad
+          alpha_1 = max_torque/J_total;
+
+          w_1 = sqrt(abs(2*alpha_1*arm_swing_angle_rad*gear_ratio));
+          w_2 = w_1/gear_ratio;
+          
+          w_launch = w_2;
+          v_launch = w_launch*cg_ball;
+          launch_angle_deg = 90-(180-arm_end_angle);
+          v_x_launch = abs(v_launch*cosd(launch_angle_deg));
+          v_y_launch = abs(v_launch*sind(launch_angle_deg));
+          
+          launch_x_y_vel = [v_x_launch, v_y_launch];
+           
+       end
+       
+       function x_landing_time = landing_distance_and_time(self, vx_req, vy_req)
+         x0 = self.X0;
+         y0 = self.Y0;
+         
+         g = self.Gravity;
+         pc = [(-g*0.5) (vy_req) (y0)];
+         times = roots(pc);
+         t_landing = times(times>0);
+         x_landing = x0 + (vx_req*t_landing);
+         
+         x_landing_time = [x_landing, t_landing];
+       end
+       
+      
+      function d_vectors = x_y_d_vectors(self, v_x, v_y, t_final)
+         x0 = self.X0;
+         y0 = self.Y0;
+         g = self.Gravity;
+         freq = self.SampleRate;
+         t_vector = linspace(0, t_final, freq);
+         x = x0 + (v_x*t_vector);
+         y = y0 + (v_y*t_vector) - (0.5*g*t_vector.^2);
+         d_vectors = [x', y', t_vector'];
+      end
+      
+   end
+end
---
+++ project_simulation.m
@@ -0,0 +1,71 @@
+close all
+clc
+clear all
+
+%% parameters from class Calculations used for simulink, script
+calc = Calculations;
+m_arm = calc.MassArm;
+cg_ball = calc.CGBall;
+r_ball = calc.RadiusBall;
+
+%% specify how much to swing the arm and rest position
+max_torque = -2.7;
+arm_swing_angle = -45; %degrees (rotating clockwise, maximum start at 180 degrees) 
+arm_start_angle = 180; 
+
+%% gear sizes radius % (cm)
+base_gear = 0.9525;
+follower_gear = 3.81;
+gear_ratio = 4;
+
+%% start positions at ball launch from origin, used for simulink, script
+rotation_pivot_height = 4.5; %(cm)
+z_distance_arm = 6.5; %cm
+
+calc.X0 = cg_ball*cosd(arm_start_angle + arm_swing_angle) + (-follower_gear/100); % initial x position (of ball)(m)
+calc.Y0 = cg_ball*sind(arm_start_angle + arm_swing_angle) + ((rotation_pivot_height+1.05+r_ball)/100) ; % initial y position (of ball)(m)
+
+%% calculate required launch velocity from torque and starting and swing angle
+v_x_y_launch = calc.launch_x_y_velocity(max_torque, arm_swing_angle, arm_start_angle, gear_ratio);
+
+vx_launch = v_x_y_launch(1);
+vy_launch = v_x_y_launch(2);
+
+%% calculate landing x distance and time of landing of ball
+x_landing_time = calc.landing_distance_and_time(vx_launch , vy_launch);
+
+x_landing = x_landing_time(1);
+t_landing = x_landing_time(2);
+
+sim("simscape_main.slx", 2.5)
+
+%% create vectors from calculated data:
+d_vectors = calc.x_y_d_vectors(vx_launch, vy_launch, t_landing);
+x_vector = d_vectors(:, 1);
+y_vector = d_vectors(:, 2);
+
+%% obtain simscape data
+% Need to offset x and y data as they start at zero due to a different
+% joint keeping track of motion and thus another reference frame.
+x_data = position_x.Data + calc.X0;
+y_data = position_y.Data + calc.Y0;
+y_data = y_data(y_data>0);
+x_data = x_data(1:length(y_data));
+x_data_landing = x_data(end);
+y_data_max = max(y_data);
+t_data = position_x.Time;
+t_data = t_data(1:length(y_data));
+t_data_landing = t_data(end);
+
+%% plot calculation vs simulation
+
+plot(x_vector, y_vector, 'o')
+hold on 
+plot(x_data, y_data)
+hold off
+
+
+
+
+
+
