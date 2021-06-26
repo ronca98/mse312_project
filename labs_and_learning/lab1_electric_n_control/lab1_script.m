c892238a29e28e5d2456623cee985c26525936a5
ronca <roncalara@outlook.com>
2021-06-26 15:26:26 -0700

    Part C uploaded of electrical lab 1

--- labs_and_learning/lab1_electric_n_control/lab1_script.m
+++ labs_and_learning/lab1_electric_n_control/lab1_script.m
@@ -2,21 +2,33 @@ clear all
 close all
 clc
 
-%% input voltage and PWM
-pwm_ref_voltage = 2.5; % V
+%% input voltage, PWM and H-Bridge
+command_voltage = 27.5; % V
 pwm_freq = 4000; % Hz
-
+Vm_min = 10;
+Vm_max = 45;
+max_voltage_input_hbridge = 5;
+voltage_reverse = 1; 
+if command_voltage < 0
+    voltage_reverse = 2.6;
+    command_voltage = abs(command_voltage);
+end
 
 %% DC motor
 % Electrical 
 % Option 1: model param: by rated load and speed
-armature_inductance = 0.01; % H
-no_load_speed = 4000; % rpm
-rated_speed = 2500; % rpm
-rated_load_power = 10; %W
 rated_dc_supply_voltage = 12; %V
+armature_inductance = 0.00234; % H
+no_load_speed = 720; % rpm
+stall_torque = 3.0e-01;
+rated_speed = 650; % rpm
+rated_load_power = 3.5; %W
 
 % Mechanical 
-rotor_inertia = 2000; % g*cm^2
-rotor_damping = 1e-06; % N*m (rad/s)
+rotor_inertia = 1.6e-06; % kg*m^2
+rotor_damping = 1.4e-06; % N*m/s(rad/s)
+gear_ratio = 5;
+
+%% run simulation
+sim('DC_motor_model.slx', 10)
 
