cd0a13f72258e2f83c2772c9978ac96d85ad9f1c
ronca <roncalara@outlook.com>
2021-07-12 15:58:39 -0700

    position control functioning with more graphs

--- electrical_params.m
+++ electrical_params.m
@@ -22,32 +22,36 @@ L_m = 0.00234; % armature inductance H
 no_load_speed = 720; % rpm
 stall_torque = 0.3; % N*m
 torque_const = 2.18e-02; % N*m/A
+back_emf_const = 2.18e-02;
 
 % Mechanical 
 system_inertia = 2.4933e-04; % kg*m^2
-rotor_inertia = 6.3^2*1.6e-06;  %+ system_inertia; % kg*m^2
+rotor_inertia = 6.3^2*1.6e-06; % + system_inertia; % kg*m^2
 rotor_damping = 1.4e-06; % N*m/s(rad/s)
 % gear ratio for external gears 
 gear_ratio_int = 6.3;
 gear_ratio_ext = 1;
 train_ratio = gear_ratio_int*gear_ratio_ext;
  
-%% PI Controller Design for Current
-a = 1;
-sample_bw_rad = 2*pi*64;
-k_p = sample_bw_rad*a*L_m;
-k_i = sample_bw_rad*R_m*a;
-
-k_p_w = ((sample_bw_rad*rotor_inertia)/10);
-k_i_w = ((sample_bw_rad*rotor_damping)/10);
+%% Controllers parameters
+sample_freq = 128;
+sample_bw_rad = 2*pi*sample_freq;
+% Current
+k_p = (sample_bw_rad*L_m/10)*12;
+k_i = (sample_bw_rad*R_m/10)*0.1;
+% Speed
+k_p_w = ((sample_bw_rad*rotor_inertia)/100)*15;
+k_i_w = ((sample_bw_rad*rotor_damping)/100)*0.1;
+% Position
+k_p_p = 10;
 
 % reference signal
 % i_ref = 0.6; % A
 w_ref = 65; % omega
-% pos_d = 45;
+pos_d = 45;
 
 %% run simulation
-model = sim("electrical_model.slx", 3);
+model = sim("electrical_model.slx", 10);
 
 %% Load data from Simulink
 time_vector = model.speed_rpm.Time;
@@ -56,17 +60,41 @@ current_vector = model.current.Data;
 power_vector = model.power.Data;
 voltage_vector = model.voltage.Data;
 
+position_ref_vector = model.position_deg_ref.Data;
 position_vector = model.position_deg.Data;
 speed_vector = model.speed_rpm.Data;
 driving_torque_vector = model.driving_torque.Data;
 
 %% Plot Data
-
+% Various Electrical Plots
 figure;
 subplot(2, 2, 1); plot(time_vector, power_vector, 'LineWidth', 2); grid on;
+title("Power (W)")
 subplot(2, 2, 2); plot(time_vector, current_vector, 'LineWidth', 2); grid on;
+title("Current (A)")
 subplot(2, 2, 3); plot(time_vector, voltage_vector, 'LineWidth', 2); grid on;
+title("Voltage (V)")
 subplot(2, 2, 4); plot(time_vector, speed_vector, 'LineWidth', 2); grid on;
+title("Speed (RPM)")
+
+% Reference vs Actual Position
+figure
+plot(time_vector, position_ref_vector);
+hold on
+plot(time_vector, position_vector);
+hold off
+legend("position reference", "position actual");
+ylabel("\theta ")
+xlabel("time (s)")
+
+% Plot Errors
+figure;
+subplot(2, 2, 1); plot(time_vector, model.error_position.Data, 'LineWidth', 2); grid on;
+title("Position Error")
+subplot(2, 2, 2); plot(time_vector, model.error_speed.Data, 'LineWidth', 2); grid on;
+title("Speed Error")
+subplot(2, 2, 3); plot(time_vector, model.error_current.Data, 'LineWidth', 2); grid on;
+title("Current Error")
 
 %% Obtain calculations from Data
 data_array = cat(2, ...
@@ -93,21 +121,21 @@ launch_driving_torque = data_array(end, 7);
 mean_driving_torque = mean(data_array(:, 7));
 
 %% display information for user
-disp("electrical_params.m:")
-electrical_info = ["Peak Current: ", peak_current, ... 
-                   "Power: ", launch_power, ...
-                   "Voltage: ", launch_voltage];
-disp(electrical_info)
-
-rotation_direction_text = "CCW";
-if command_voltage < 0
-    rotation_direction_text = "CW";
-end
+% disp("electrical_params.m:")
+% electrical_info = ["Peak Current: ", peak_current, ... 
+%                    "Power: ", launch_power, ...
+%                    "Voltage: ", launch_voltage];
+% disp(electrical_info)
+% 
+% rotation_direction_text = "CCW";
+% if command_voltage < 0
+%     rotation_direction_text = "CW";
+% end
                
-mechanical_info = ["Launch time:", launch_time, ...
-                   "Launch angle: ", launch_angle, ...
-                   "Launch_speed: ", launch_speed];          
-disp(mechanical_info)
+% mechanical_info = ["Launch time:", launch_time, ...
+%                    "Launch angle: ", launch_angle, ...
+%                    "Launch_speed: ", launch_speed];          
+% disp(mechanical_info)
 
 % torque_info = ["Peak Driving Torque", peak_driving_torque, ...
 %                "Launch Driving Torque",  launch_driving_torque, ...
