0813cfdb25aa62962927c5ff3d9c9f372f59b345
ronca <roncalara@outlook.com>
2021-07-13 21:32:04 -0700

    improved speed control parameters

--- electrical_params.m
+++ electrical_params.m
@@ -26,51 +26,50 @@ back_emf_const = 2.18e-02;
 
 % Mechanical 
 system_inertia = 2.4933e-04; % kg*m^2
-rotor_inertia = 6.3^2*1.6e-06; % + system_inertia; % kg*m^2
+rotor_inertia = 6.3^2*1.6e-06 + system_inertia; % kg*m^2
 rotor_damping = 1.4e-06; % N*m/s(rad/s)
 % gear ratio for external gears 
 gear_ratio_int = 6.3;
-gear_ratio_ext = 1;
+gear_ratio_ext = 4.8;
 train_ratio = gear_ratio_int*gear_ratio_ext;
  
 %% Controllers parameters
 sample_freq = 128;
 sample_bw_rad = 2*pi*sample_freq;
 % Current
-k_p = (sample_bw_rad*L_m/10);
-k_i = (sample_bw_rad*R_m/10);
+k_p = (sample_bw_rad*L_m/10)*1;
+k_i = (sample_bw_rad*R_m/10)*3;
 % Speed
-k_p_w = ((sample_bw_rad*rotor_inertia)/100)*20;
+k_p_w = ((sample_bw_rad*rotor_inertia)/100)*150;
 k_i_w = ((sample_bw_rad*rotor_damping)/100);
 % Position
 k_p_p = 45;
 k_i_p = 1;
 k_d_p = 0.01;
-
 % reference signal
-% i_ref = 0.6; % A
-w_ref = 720; % omega
 pos_d = 45; % degrees
+speed_ramp_t = 0.10;
+w_d = (pos_d/speed_ramp_t)*(pi/180)*9.55 * 0.8; % rpm
+t_final = 1.5;
+period = (1/sample_freq)*0.01;
 
-%% generate input signal
-t_final = 0.1;
-period = (1/sample_freq)*0.1;
-% parabolic
-time_phase_one = 0:period:0.02;
-y_phase_one =  45000*time_phase_one.^2;
-% ramp
-time_phase_two = (0.02+period):period:0.05;
-y_phase_two = ((pos_d-y_phase_one(end))/(0.03-period))*time_phase_two;
-% quintic
+%% generate input signal for speed
+time_phase_one = 0:period:speed_ramp_t;
+y_phase_one = (w_d/speed_ramp_t)*(time_phase_one);
+time_phase_two = (speed_ramp_t+period):period:t_final;
+y_phase_two = y_phase_one(end)*ones(1, length(time_phase_two));
+input_time = cat(1, time_phase_one', time_phase_two');
+input_y = cat(1, y_phase_one', y_phase_two');
+
+w_ref = [input_time, input_y];
+
+%% generate input signal for position
 time_pts_phase_three = [(0.05+period), 0.2, 0.4, 0.6, 0.8, 1, t_final];
 y_pts_phase_three = [pos_d, 30, 15, 10, 4, 2, 0];
 time_phase_three = (0.05+period):period:t_final;
 quintic_polynomial = polyfit(time_pts_phase_three, y_pts_phase_three, 5);
 y_phase_three = polyval(quintic_polynomial, time_phase_three);
-% combine into array
-input_time = cat(1, time_phase_one', time_phase_two', time_phase_three');
-input_y = cat(1, y_phase_one', y_phase_two', y_phase_three');
-pos_ref = [input_time, input_y];
+pos_ref = [time_phase_three', y_phase_three'];
 
 %% run simulation
 model = sim("electrical_model.slx", t_final);
@@ -82,7 +81,7 @@ current_vector = model.current.Data;
 power_vector = model.power.Data;
 voltage_vector = model.voltage.Data;
 
-%position_ref_vector = model.position_deg_ref.Data;
+speed_ref_vector = model.speed_rpm_ref.Data;
 position_vector = model.position_deg.Data;
 speed_vector = model.speed_rpm.Data;
 driving_torque_vector = model.driving_torque.Data;
@@ -99,15 +98,15 @@ title("Voltage (V)")
 subplot(2, 2, 4); plot(time_vector, speed_vector, 'LineWidth', 2); grid on;
 title("Speed (RPM)")
 
-% Reference vs Actual Position
-% figure
-% plot(time_vector, position_ref_vector);
-% hold on
-% plot(time_vector, position_vector);
-% hold off
-% legend("position reference", "position actual");
-% ylabel("\theta ")
-% xlabel("time (s)")
+% Reference vs Actual Speed
+figure
+plot(time_vector, speed_ref_vector);
+hold on
+plot(time_vector, speed_vector);
+hold off
+legend("speed reference", "speed actual");
+ylabel("\theta ")
+xlabel("time (s)")
 
 % Plot Errors
 figure;
@@ -122,7 +121,7 @@ title("Current Error")
 data_array = cat(2, ...
                  power_vector, current_vector, voltage_vector, ...
                  time_vector, position_vector, speed_vector, driving_torque_vector);            
-swing_angle = 75;
+swing_angle = pos_d;
 filtered_current_vector = data_array(:, 2);
 [~, idx] = max(abs(filtered_current_vector));
 peak_current = filtered_current_vector(idx);
@@ -143,27 +142,27 @@ launch_driving_torque = data_array(end, 7);
 mean_driving_torque = mean(data_array(:, 7));
 
 %% display information for user
-% disp("electrical_params.m:")
-% electrical_info = ["Peak Current: ", peak_current, ... 
-%                    "Power: ", launch_power, ...
-%                    "Voltage: ", launch_voltage];
-% disp(electrical_info)
-% 
-% rotation_direction_text = "CCW";
-% if command_voltage < 0
-%     rotation_direction_text = "CW";
-% end
-               
-% mechanical_info = ["Launch time:", launch_time, ...
-%                    "Launch angle: ", launch_angle, ...
-%                    "Launch_speed: ", launch_speed];          
-% disp(mechanical_info)
+disp("electrical_params.m:")
+electrical_info = ["Peak Current: ", peak_current, ... 
+                   "Power: ", launch_power, ...
+                   "Voltage: ", launch_voltage];
+disp(electrical_info)
 
-% torque_info = ["Peak Driving Torque", peak_driving_torque, ...
-%                "Launch Driving Torque",  launch_driving_torque, ...
-%                rotation_direction_text, ...
-%                "Mean Driving Torque", mean_driving_torque];
-% disp(torque_info)
+rotation_direction_text = "CCW";
+if command_voltage < 0
+    rotation_direction_text = "CW";
+end
+               
+mechanical_info = ["Launch time:", launch_time, ...
+                   "Launch angle: ", launch_angle, ...
+                   "Launch_speed: ", launch_speed];          
+disp(mechanical_info)
+
+torque_info = ["Peak Driving Torque", peak_driving_torque, ...
+               "Launch Driving Torque",  launch_driving_torque, ...
+               rotation_direction_text, ...
+               "Mean Driving Torque", mean_driving_torque];
+disp(torque_info)
 
 
 
